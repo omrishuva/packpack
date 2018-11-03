8c7be7f55a66a0bcfd56899b2102c70ed893c1ef
Omri Shuva <omrishuva1@gmail.com>
2018-11-04 00:00:31 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='packpack',
+      version='0.1',
+      description='python package template',
+      url='https://github.com/omrishuva/packpack',
+      author='Omri Shuva',
+      author_email='omrishuva1@gmail.com',
+      license='MIT',
+      packages=['packpack'],
+      zip_safe=False)
